d0c68731c827a1cb9d05df1b8acde5d827a88f29
csrdg <cesardg.pb@gmail.com>
2024-01-17 06:45:34 +0100

    iteración#3

--- JS/es6/app.js
+++ JS/es6/app.js
@@ -70,3 +70,40 @@ const [año1, año2, año3] = itv;
 console.log("🚀 ~ año1:", año1);
 console.log("🚀 ~ año2:", año2);
 console.log("🚀 ~ año3:", año3);
+
+// **Iteración #3: Spread Operator**
+
+// 3.1 Dado el siguiente array, crea una copia usando spread operators.
+const pointsList = [32, 54, 21, 64, 75, 43];
+const CopypointsList = [...pointsList];
+console.log("🚀 ~ CopypointsList:", CopypointsList);
+
+// 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+const copyToy = { ...toy };
+console.log("🚀 ~ copyToy:", copyToy);
+
+// 3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
+// spread operatos.
+const pointsListOne = [32, 54, 21, 64, 75, 43];
+const pointsLis2Two = [54, 87, 99, 65, 32];
+
+const concatPointsList = [...pointsListOne, ...pointsLis2Two];
+console.log("🚀 ~ concatPointsList:", concatPointsList);
+
+// 3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
+// con spread operators.
+const toyOne = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
+const toyUpdate = { lights: "rgb", power: ["Volar like a dragon", "MoonWalk"] };
+
+const concatToy = { ...toyOne, ...toyUpdate };
+console.log("🚀 ~ concatToy:", concatToy);
+
+// 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
+// pero sin editar el array inicial. De nuevo, usando spread operatos.
+
+const colors = ["rojo", "azul", "amarillo", "verde", "naranja"];
+
+const menosColors = [...colors];
+menosColors.splice(2, 1);
+console.log("🚀 ~ menosColors:", menosColors);
