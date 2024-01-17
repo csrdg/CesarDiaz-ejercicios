7ecf97e1913607ed06f06bc78a71b65fc1572adf
csrdg <cesardg.pb@gmail.com>
2024-01-17 06:29:04 +0100

    iteración2

--- JS/es6/app.js
+++ JS/es6/app.js
@@ -16,3 +16,57 @@ const sumParam = (a = 10, b = 5) => {
 const sinParam = sumParam();
 const conUnParam = sumParam(1);
 const conDosParam = sumParam(1, 1);
+
+// **Iteración #2: Destructuring**
+
+// 2.1 En base al siguiente javascript, crea variables en base a las propiedades
+// del objeto usando object destructuring e imprimelas por consola. Cuidado,
+// no hace falta hacer destructuring del array, solo del objeto.
+
+const game = {
+  title: "The last us 2",
+  gender: ["action", "zombie", "survival"],
+  year: 2020,
+};
+
+const { title, gender, year } = game;
+console.log("🚀 ~ year:", year);
+console.log("🚀 ~ gender:", gender);
+console.log("🚀 ~ title:", title);
+
+// 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables
+// llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
+// imprimelo por consola.
+
+const fruits = ["Banana", "Strawberry", "Orange"];
+
+const [fruit1, fruit2, fruit3] = fruits;
+console.log("🚀 ~ fruit1:", fruit1);
+console.log("🚀 ~ fruit2:", fruit2);
+console.log("🚀 ~ fruit3:", fruit3);
+
+// 2.3 En base al siguiente javascript, usa destructuring para crear 2
+// variables igualandolo a la función e imprimiendolo por consola.
+
+const animalFunction = () => {
+  return { name: "Bengal Tiger", race: "Tiger" };
+};
+
+// const { name, race } = animalFunction();
+// console.log("🚀 ~ name:", name);
+// console.log("🚀 ~ race:", race);
+
+// 2.4 En base al siguiente javascript, usa destructuring para crear las
+// variables name y itv con sus respectivos valores. Posteriormente crea
+// 3 variables usando igualmente el destructuring para cada uno de los años
+// y comprueba que todo esta bien imprimiendolo.
+
+const car = { name: "Mazda 6", itv: [2015, 2011, 2020] };
+
+const { name, itv } = car;
+console.log("🚀 ~ name:", name);
+console.log("🚀 ~ itv:", itv);
+const [año1, año2, año3] = itv;
+console.log("🚀 ~ año1:", año1);
+console.log("🚀 ~ año2:", año2);
+console.log("🚀 ~ año3:", año3);
